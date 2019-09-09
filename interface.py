5e9074c2ed27dba03426c1bb53836fa7ddfba5e9
Sridaran <sri7thon@gmail.com>
2019-09-08 20:43:12 -0400

    Add base functionality

--- interface.py
+++ interface.py
@@ -1,7 +1,34 @@
+import requests
+import json
+
 class DictionaryAPIInterface:
     def __init__(self):
-        pass
+        self.mode = 't'
+        with open('keys.json', 'r') as keys_file:
+            json_text = keys_file.read()
+        self.keys_dict = json.loads(json_text)
+        self.session = requests.Session()
     def request(self, word):
-        pass
+        key = self.keys_dict['dict-key'] if self.mode == 'd' else self.keys_dict['thes-key']
+        response = self.session.get(f'https://www.dictionaryapi.com/api/v3/references/collegiate/json/voluminous?key={key}')
+        try:
+            response = response.json()[0]
+        except Exception as e:
+            return e
+        word_class = response['fl']
+        definitions_base = response['def'][0]['sseq']
+        definitions = list()
+        for part in definitions_base:
+            for section in part:
+                definitions.append(section[1]['dt'][0][1])
+        output = f'{word}, {word_class}\n'
+        for definition in definitions:
+            output += definition
+            output += '\n'
+        return output
     def set_mode(self, mode):
-        pass
+        self.mode = mode
+
+x = DictionaryAPIInterface()
+x.set_mode('d')
+print(x.request('hello'))
